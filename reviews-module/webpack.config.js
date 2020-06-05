09c0cc98805a718d8dd34faf98019b2c012409f8
Kin Tat Lay <hongkongbboy@gmail.com>
2020-06-05 02:24:28 -0700

    Corrected webpack

--- reviews-module/webpack.config.js
+++ reviews-module/webpack.config.js
@@ -1,18 +1,15 @@
 /********  Webpack Production  ********/
-const path = require('path');
+var path = require('path');
+var SRC_DIR = path.join(__dirname, 'client', 'src');
+var DIST_DIR = path.join(__dirname, 'client', 'dist');
 const CompressionPlugin = require('compression-webpack-plugin');
-
 const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
-
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
-
 module.exports = {
-  mode: 'production',
-  entry: ['babel-polyfill', `${SRC_DIR}/index.jsx`],
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR,
+    path: DIST_DIR
   },
   plugins: [
     new BrotliGzipPlugin({
@@ -39,21 +36,20 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        include: SRC_DIR,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: "babel-loader"
+        }
       },
-    ],
-  },
-  devServer: {
-    watchContentBase: true,
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.es6'],
+    extensions: [".js", ".json", ".jsx", ".css"],
   },
 };
-
 
 /********  Webpack Development  ********/ 
 // var path = require('path');
